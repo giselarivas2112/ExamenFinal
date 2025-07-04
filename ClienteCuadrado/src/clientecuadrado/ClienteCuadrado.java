9931f3dc8c9a0f86422b77256c8153c2f7877fd3
giselarivas2112 <00016124@uca.edu.sv>
2025-07-04 13:00:19 -0600

    Agregar codigo a clase ClienteCuadrado

--- ClienteCuadrado/src/clientecuadrado/ClienteCuadrado.java
+++ ClienteCuadrado/src/clientecuadrado/ClienteCuadrado.java
@@ -4,17 +4,35 @@
  */
 package clientecuadrado;
 
-/**
- *
- * @author Owner
- */
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClienteCuadrado {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+
+        try (Socket socket = new Socket("localhost", 5000)) {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Ingresa tu nombre: ");
+            String nombre = scanner.nextLine();
+
+            System.out.print("Ingresa un número entero: ");
+            int numero = scanner.nextInt();
+
+            salida.println(nombre);
+            salida.println(numero);
+
+            System.out.println(entrada.readLine());
+            System.out.println(entrada.readLine());
+            System.out.println(entrada.readLine());
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
-    
 }
